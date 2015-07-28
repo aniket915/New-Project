f284990df6c84567a69da0909067f09c33c6a953
aniket915 <aniket.mendhule@firstcry.com>
2015-07-28 15:05:28 +0530

    28072015 15.05
    Added

---
+++ TestDemo/testWeb/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace testWeb
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string message = "Order Placed Successfully.";
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            sb.Append("<script type = 'text/javascript'>");
+
+            sb.Append("window.onload=function(){");
+
+            sb.Append("alert('");
+
+            sb.Append(message);
+
+            sb.Append("')};");
+
+            sb.Append("</script>");
+
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", sb.ToString());
+        }
+    }
+}
